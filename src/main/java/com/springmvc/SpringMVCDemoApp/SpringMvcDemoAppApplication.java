d58c512556c6e7e32b0f7d4973c9504a4b0b3f4a
MOHITE RAJ <mohiteraj647@gmail.com>
2024-09-12 12:31:48 +0530

    Added variable to Feature-300

--- src/main/java/com/springmvc/SpringMVCDemoApp/SpringMvcDemoAppApplication.java
+++ src/main/java/com/springmvc/SpringMVCDemoApp/SpringMvcDemoAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcDemoAppApplication {
 
+	int num = 10;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcDemoAppApplication.class, args);
 	}
